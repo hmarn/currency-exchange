aa6df298f9bdc5c08bc7af3208a2ca573b3edbbe
Hay Marn <haymarn03@gmail.com>
2023-01-02 19:01:00 +0800

    Update rates API URL

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,8 @@ import CurrencyTable from './components/CurrencyTable';
 import CurrencyConverter from './components/CurrencyConverter';
 import axios from 'axios';
 
-const URL = 'http://localhost:8000/exchange_rate';  
+// const URL = 'http://localhost:8000/exchange_rate';
+const URL = 'https://openexchangerates.org/api/latest.json?app_id=5fcc6db49fdf45cb959de28cce648af2'
 const App = () => {
   const [data, setData] = useState([]);
 
